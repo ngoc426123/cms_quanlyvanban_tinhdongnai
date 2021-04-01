78e759f20b46f300f1d3186d872e2512093afc2d
Hoang Minh Ngoc <ngoc.hm@sutrixsolutions.com>
2021-04-01 09:49:22 +0700

    09:50 01/04/2021: Add sub menu to sidebar

--- src/js/plugins/sidebar.js
+++ src/js/plugins/sidebar.js
@@ -3,6 +3,11 @@
     dataToggle: '[data-toggle]',
     dataOverlay: '[data-overlay]',
     clsOpen: 'open',
+
+    dataMenu: '[data-menu-item]',
+    dataMenuToggle: '[data-menu-toggle]',
+    dataMenuSubmenu: '[data-menu-submenu]',
+    clsActive: 'active',
   }
 })
 export default class Sidebar {
@@ -14,11 +19,13 @@ export default class Sidebar {
   initDom () {
     const {
       dataToggle,
-      dataOverlay
+      dataOverlay,
+      dataMenuToggle
     } = this.options;
 
     this.$toggle = this.$element.find(dataToggle);
     this.$overlay = this.$element.find(dataOverlay);
+    this.$menuToggle = this.$element.find(dataMenuToggle);
   }
 
   handleEvent () {
@@ -29,6 +36,11 @@ export default class Sidebar {
     this.$overlay
       .off('click')
       .on('click', () => this.onClickOverlay());
+
+    this.$menuToggle
+      .off('click')
+      .on('click', (event) => this.onClickMenu(event));
+
   }
 
   onClickToggle () {
@@ -43,5 +55,24 @@ export default class Sidebar {
     const { clsOpen } = this.options;
 
     this.$element.removeClass(clsOpen);
+  }
+
+  onClickMenu (event) {
+    const {
+      clsActive,
+      dataMenu,
+      dataMenuSubmenu
+    } = this.options;
+    const target = $(event.currentTarget);
+    const parent = target.parents(dataMenu);
+    const submenu = parent.find(dataMenuSubmenu);
+    const isActive = parent.hasClass(clsActive);
+
+    parent.siblings(dataMenu).removeClass(clsActive);
+    parent.siblings(dataMenu).find(dataMenuSubmenu).stop().slideUp(300);
+    !isActive && parent.addClass(clsActive);
+    !isActive && submenu.stop().slideDown(300);
+    isActive && parent.removeClass(clsActive);
+    isActive && submenu.stop().slideUp(300);
   }
 }
